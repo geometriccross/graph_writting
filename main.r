5702d71909bd03ad3ec6551988350b00e0a5d8d7
geometriccross <geometriccross@desktop.>
2025-11-27 14:55:56 +0900

    re-write metadata

--- main.r
+++ main.r
@@ -13,8 +13,24 @@ pseq <- qza_to_phyloseq(
     tax_fix() %>%
     tax_fix(unknowns = c("uncultured"))
 
+metadata <- read_tsv("./q2objs/metadata.tsv", comment = "") %>%
+    rename(SampleID = `#SampleID`) %>%
+    column_to_rownames("SampleID")
+
+sample_data(pseq) <- sample_data(metadata)
+
+print(colnames(sample_data(pseq)))
+
 p <- pseq %>%
-    comp_barplot(tax_level = "Genus") +
+    ps_arrange("RawID") %>%
+    comp_barplot(
+        tax_level = "Genus",
+        n_taxa = 32,
+        bar_outline_colour = NA,
+        sample_order = "asis",
+        label = "RawID"
+    ) +
     coord_flip()
+
 
 ggsave("barplot.svg", plot = p, width = 10, height = 8, dpi = 300)
