c4ac4ba8efced8923800b61188ee22b027d09e3a
geometriccross <geometriccross@desktop.>
2025-12-09 17:53:20 +0900

    関数の位置を移動

--- src/barplot.r
+++ src/barplot.r
@@ -6,6 +6,23 @@ library(gtools)
 
 source("./src/q2obj.r")
 
+
+write_plot <- function(ps, filename, width = 16, height = 9, dpi = 600) {
+    plot_obj <- comp_barplot(
+        ps,
+        tax_level = "Genus",
+        sample_order = natural_sorted,
+        label = "RawID",
+        n_taxa = 41, # brewerPlusは最大個まで色分けできる
+        palette = distinct_palette(n = 41, pal = "brewerPlus", add = "gray"),
+        bar_outline_colour = NA,
+        bar_width = 0.9,
+    ) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+    ggsave(filename, plot = plot_obj, width = width, height = height, dpi = dpi)
+}
+
+
 phyloseq_object <- load_q2obj() %>% tax_tweak()
 
 natural_sorted <- phyloseq_object %>%
@@ -27,17 +44,5 @@ exclude_patterns <- c(
 phyloseq_filtered <- phyloseq_object %>%
     subset_taxa(!grepl(exclude_patterns, Genus))
 
-composition_barplot <- phyloseq_filtered %>%
-    comp_barplot(
-        tax_level = "Genus",
-        sample_order = natural_sorted,
-        label = "RawID",
-        n_taxa = 41, # brewerPlusは最大個まで色分けできる
-        palette = distinct_palette(n = 41, pal = "brewerPlus", add = "gray"),
-        bar_outline_colour = NA,
-        bar_width = 0.9,
-    ) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
-
-ggsave("barplot.svg", plot = composition_barplot, width = 16, height = 9, dpi = 600)
+write_plot(phyloseq_filtered, "./barplot_genus_filtered.svg")
