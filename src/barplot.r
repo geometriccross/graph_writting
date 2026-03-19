a8521e8c5c42273bbc7e9813077be75a0f0d56fd
geometriccross <geometriccross@desktop.>
2026-03-19 16:25:14 +0900

    カラーパレットを最適化

--- src/barplot.r
+++ src/barplot.r
@@ -7,14 +7,21 @@ library(gtools)
 source("./src/q2obj.r")
 
 
-write_plot <- function(ps, filename, width = 16, height = 9, dpi = 600) {
+write_plot <- function(ps, filename, tax_order = sum, width = 16, height = 9, dpi = 600) {
+    n_taxa_value <- if (is.function(tax_order)) 41 else length(tax_order)
+
+    color_pal <- distinct_palette(
+        n = n_taxa_value, pal = "brewerPlus", add = "gray"
+    )
+
     plot_obj <- comp_barplot(
         ps,
         tax_level = "Genus",
         sample_order = natural_sorted,
         label = "RawID",
-        n_taxa = 41, # brewerPlusは最大個まで色分けできる
-        palette = distinct_palette(n = 41, pal = "brewerPlus", add = "gray"),
+        n_taxa = n_taxa_value, # brewerPlusは最大個まで色分けできる
+        tax_order = tax_order,
+        palette = color_pal,
         bar_outline_colour = NA,
         bar_width = 0.9,
     ) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
@@ -46,3 +53,34 @@ phyloseq_filtered <- phyloseq_object %>%
 
 
 write_plot(phyloseq_filtered, "./barplot_genus_filtered.svg")
+
+# 細胞内共生細菌のみを表示(それ以外はOtherとして集約)
+intracellular_taxa <- c(
+    "Bartonella",
+    "Coxiella",
+    "Rickettsia",
+    "Anaplasma",
+    "Ehrlichia",
+)
+
+write_plot(
+    "./barplot_intracellular.svg",
+    tax_order = c(intracellular_taxa, "Other")
+)
+
+symbiotic_taxa <- c(
+    "Wolbachia",
+    "Spiroplasma",
+    "Rickettsiella",
+    "Arsenophonus",
+    "Cardinium",
+    "Hamiltonella",
+    "Regiella",
+    "Sodalis",
+    "Serratia",
+    "Buchnera",
+    "Blochmannia",
+    "Baumannia",
+    "Sulcia",
+    "Nasuia"
+)
