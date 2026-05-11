6ce62d1dcfd1be3d533d534d6a4b642b867ff982
geometriccross <geometriccross@gmail.com>
2026-05-11 15:00:37 +0900

    型チェックを追加

--- src/q2obj.r
+++ src/q2obj.r
@@ -5,6 +5,42 @@ library(qiime2R)
 
 MAX_DEPTH <- 3
 
+# .qza ファイルの QIIME2 型（type）を取得
+# .qza は ZIP アーカイブで、<uuid>/metadata.yaml に型情報が記録されている
+get_q2_type <- function(qza_path) {
+    tryCatch(
+        {
+            tmp <- tempfile()
+            dir.create(tmp)
+            on.exit(unlink(tmp, recursive = TRUE))
+
+            # read_qza と同じ手法
+            unpacked <- unzip(qza_path, list = TRUE)
+            uuid_dir <- gsub("/.*", "", unpacked$Name[1])
+            yaml_path <- file.path(tmp, uuid_dir, "metadata.yaml")
+            unzip(qza_path, files = file.path(uuid_dir, "metadata.yaml"), exdir = tmp)
+
+            lines <- readLines(yaml_path)
+            match <- str_match(paste(lines, collapse = "\n"), "type:\s*(.+?)\s*$")
+            if (is.na(match[1, 2])) stop("type field not found in metadata.yaml")
+            match[1, 2]
+        },
+        error = function(e) stop(sprintf("Failed to read q2 type from %s: %s", qza_path, e$message))
+    )
+}
+
+# .qza ファイルの型が期待値と一致するか検証
+assert_q2_type <- function(qza_path, expected_type) {
+    actual <- get_q2_type(qza_path)
+    if (actual != expected_type) {
+        stop(sprintf(
+            "QIIME2 type mismatch for %s\n  expected: %s\n  actual:   %s",
+            basename(qza_path), expected_type, actual
+        ))
+    }
+    invisible(actual)
+}
+
 # フォルダ内からパターンに一致するファイルを再帰的に探索
 find_file <- function(folder, pattern) {
     files <- list.files(
@@ -40,6 +76,12 @@ load_q2obj <- function(folder) {
     features_path <- find_file(folder, "common_biology_free_table.qza$")
     taxonomy_path <- find_file(folder, "common_biology_free_classification.qza$")
     tree_path     <- find_file(folder, "biology_free_tree.qza$")
+
+    # QIIME2 型の検証
+    assert_q2_type(features_path, "FeatureTable[Frequency]")
+    assert_q2_type(taxonomy_path, "FeatureData[Taxonomy]")
+    assert_q2_type(tree_path,     "Phylogeny[Rooted]")
+
     qza_to_phyloseq(
         features = features_path,
         taxonomy = taxonomy_path,
